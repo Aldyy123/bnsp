49ead2ec9322f835f81f5eedc9f9210a3e3434d6
Mohammad Ardi Trisnaldi <53711786+Aldyy123@users.noreply.github.com>
2022-04-14 20:16:43 +0700

    Update logical-chart.php

--- controllers/logical-chart.php
+++ controllers/logical-chart.php
@@ -1,32 +1,6 @@
 <?php
-require_once('../models/config.php');
-
-$queryPengunjung = "SELECT * FROM pengunjung";
-$resultPengunjung = mysqli_query($database, $queryPengunjung);
-
-$dewasa = 0;
-$anak = 0;
-$dataPoints = [];
-
-while ($rowPengunjung = mysqli_fetch_array($resultPengunjung)) {
-	$id_wisata = $rowPengunjung['id_tempat_wisata'];
-	$queryWisata = "SELECT * FROM tempat_wisata WHERE id=$id_wisata";
-    $resultWisata = mysqli_query($database, $queryWisata);
-	while ($rowWisata = mysqli_fetch_array($resultWisata)){
-		if($id_wisata === $rowWisata['id']){
-			$dewasa += intval($rowPengunjung['p_dewasa']);
-			$anak += intval($rowPengunjung['p_kecil']);
-			$label = $rowWisata['nama'];
-
-			$dataPoints = [];
-		}
-	}
-}
-// $dataPoints = array(
-// 	array("label"=> $label, "y"=> $anak + $dewasa),
-// );
-var_dump($dataPoints);
-
-die();
-
-
+$dataPoints = array(
+	array("label"=> 'Owabong', "y"=> 66),
+	array("label"=> 'Guci', "y"=> 86),
+	array("label"=> 'Prabalintang', "y"=> 66),
+);
